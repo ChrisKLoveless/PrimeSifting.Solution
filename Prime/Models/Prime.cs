d5a5458015d2a0fd1e80a75347c29c0681b73109
ChrisKLoveless <chriskloveless@gmail.com>
2022-12-07 16:56:31 -0800

    WIP: fixing bug in printing
    co-authored-by: Yoonis Ali <yoonismahamoudali@gmail.com>

--- Prime/Models/Prime.cs
+++ Prime/Models/Prime.cs
@@ -4,23 +4,36 @@ namespace Prime.Models
 {
     public class Number 
     {
-        public int UserNumber { get; set; }
-        private static List<Number> _instances = new List<Number> { };
-
-        public Number(int num)
+        public static bool isPrime(int num) 
         {
-            UserNumber = num;
-            _instances.Add(this);
+            bool prime = true;
+            List<int> basePrime = new List<int>{2, 3, 5, 7};
+            foreach(int i in basePrime)
+            {
+                if(num % i == 0)
+                {
+                    prime = false;
+                }
+            }
+            return prime;
         }
+        // public int UserNumber { get; set; }
+        // private static List<Number> _instances = new List<Number> { };
 
-        public static List<Number> GetAll()
-        {
-            return _instances;
-        }
+        // public Number(int num)
+        // {
+        //     UserNumber = num;
+        //     _instances.Add(this);
+        // }
 
-        public static void ClearAll()
-        {
-            _instances.Clear();
-        }
+        // public static List<Number> GetAll()
+        // {
+        //     return _instances;
+        // }
+
+        // public static void ClearAll()
+        // {
+        //     _instances.Clear();
+        // }
     }
 }
--- Prime/Program.cs
+++ Prime/Program.cs
@@ -1,14 +1,52 @@
 using System;
 using Prime.Models;
+using Prime;
 using System.Collections.Generic;
 
 namespace Prime.Program
 {
-    public class Program 
+    public class Program
     {
         static void Main()
         {
-            
+            Console.WriteLine("Please input a number from 1 to 100");
+            string? stringUserInput = Console.ReadLine();
+            int userInput = 0;
+            int.TryParse(stringUserInput, out userInput);
+            Console.WriteLine("Your prime numbers are:\n");
+            for (int i = 0; i <= userInput; i++)
+            {
+                if (i > 1)
+                {
+                    bool valid = Number.isPrime(i);
+                    if (valid) 
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                
+            }
         }
     }
 }
+
+            // Number userNum = new Number(userInput);
+            // List<int> inputList = new List<int> { 2, 2 };
+            // Console.WriteLine("------------");
+
+            // for (int i = 2; i <= userInput; i++)
+            // {
+                
+            //     inputList.Add(i);
+            // }
+            // Console.WriteLine(inputList[i]);
+            // // List<int> numList = int.Parse(inputList);
+            // // List<int> outputList = new List<int> { numList };
+            // foreach (int oi in inputList)
+            // {
+            //     if (oi % 2 == 0)
+            //     {
+            //         inputList.Remove(oi);
+            //     }
+            //     Console.WriteLine(inputList[oi]);
+            // }
